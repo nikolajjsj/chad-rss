19e925202232a34fd9f1877a704c4ab7f8da58a4
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-08-10 15:13:12 +0200

    feat(routes): add initial signin/signup/404 pages

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,45 +1,164 @@
 package server
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"chad-rss/cmd/web"
+	database "chad-rss/internal/database/sqlc"
+
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/google/uuid"
+	_ "github.com/joho/godotenv/autoload"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var tokenAuth *jwtauth.JWTAuth
+
+func init() {
+	tokenAuth = jwtauth.New("HS256", []byte(os.Getenv("JWT_SECRET")), nil)
+
+	// For debugging/example purposes, we generate and print
+	// a sample jwt token with claims `user_id:123` here:
+	_, tokenString, _ := tokenAuth.Encode(map[string]interface{}{"user_id": 123})
+	fmt.Printf("DEBUG: a sample jwt is %s\n\n", tokenString)
+}
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
+	// r.Use(middleware.Heartbeat("/"))
 	r.Use(middleware.Logger)
-
-	r.Get("/", s.HelloWorldHandler)
-
-	r.Get("/health", s.healthHandler)
+	r.NotFound(templ.Handler(web.NotFound()).ServeHTTP)
 
 	fileServer := http.FileServer(http.FS(web.Files))
 	r.Handle("/assets/*", fileServer)
-	r.Get("/web", templ.Handler(web.HelloForm()).ServeHTTP)
-	r.Post("/hello", web.HelloWebHandler)
+
+	// Public routes
+	r.Group(func(r chi.Router) {
+		// r.Get("/welcome", templ.Handler(web.Welcome()).ServeHTTP)
+		r.Get("/", templ.Handler(web.Dashboard()).ServeHTTP)
+		r.Get("/about", templ.Handler(web.About()).ServeHTTP)
+
+		r.Get("/signin", templ.Handler(web.SigninForm()).ServeHTTP)
+		r.Post("/signin", s.Signin)
+
+		r.Get("/signup", templ.Handler(web.SignupForm()).ServeHTTP)
+		r.Post("/signup", s.Signup)
+	})
+
+	// Protected routes
+	r.Group(func(r chi.Router) {
+		// Seek, verify and validate JWT tokens
+		r.Use(jwtauth.Verifier(tokenAuth))
+		r.Use(jwtauth.Authenticator(tokenAuth))
+
+		r.Get("/health", s.healthHandler)
+		// r.Get("/admin", func(w http.ResponseWriter, r *http.Request) {
+		// 	_, claims, _ := jwtauth.FromContext(r.Context())
+		// 	w.Write([]byte(fmt.Sprintf("protected area. hi %v", claims["user_id"])))
+		// })
+	})
 
 	return r
-}
-
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
 }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
 	jsonResp, _ := json.Marshal(s.db.Health())
 	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) PingHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("pong"))
+}
+
+func (s *Server) Signup(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Parse form error: ", err)
+		return
+	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 8)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Hash password error: ", err)
+		return
+	}
+
+	if _, err = s.db.Query().CreateUser(context.Background(), database.CreateUserParams{Username: username, Password: string(hashedPassword)}); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Create user in DB error:", err)
+		return
+	}
+
+	_, tokenString, err := tokenAuth.Encode(map[string]interface{}{
+		"username":      username,
+		"refresh_token": uuid.New().String(),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error creating JWT", err)
+		return
+	}
+	r.AddCookie(&http.Cookie{
+		Name:     "jwt",
+		Value:    tokenString,
+		Expires:  time.Now().Add(24 * time.Hour),
+		Secure:   true,
+		HttpOnly: true,
+	})
+}
+
+func (s *Server) Signin(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	user, err := s.db.Query().GetUserByUsername(context.Background(), username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Compare the stored hashed password, with the hashed version of the password that was received
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Create a new token
+	_, tokenString, err := tokenAuth.Encode(map[string]interface{}{
+		"username":      username,
+		"refresh_token": uuid.New().String(),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	r.AddCookie(&http.Cookie{
+		Name:     "jwt",
+		Value:    tokenString,
+		Expires:  time.Now().Add(24 * time.Hour),
+		Secure:   true,
+		HttpOnly: true,
+	})
 }
