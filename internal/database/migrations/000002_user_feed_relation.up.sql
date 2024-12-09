68aee5213261681b1db543f4954014ecf07d7c27
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-12-09 17:56:25 +0100

    booooooooom

--- internal/database/migrations/000002_user_feed_relation.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_feed (
-  user_id INTEGER NOT NULL,
-  feed_id INTEGER NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(feed_id) REFERENCES feeds(id),
-  PRIMARY KEY (user_id, feed_id)
-);
-
--- internal/utils/nid.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import gonanoid "github.com/matoous/go-nanoid/v2"
-
-func GenerateNID() (string, error) {
-	return gonanoid.New()
-}
